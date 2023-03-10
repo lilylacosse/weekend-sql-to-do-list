690224b0b50a6abe90076d58d1888a922cc803bb
Lily LaCosse <lilylacosse@gmail.com>
2023-03-10 15:48:02 -0600

    updated database.sql with information to create db and table

---
+++ database.sql
@@ -0,0 +1,16 @@
+-- Run Create Database command to create a database with the same name as the one coded into the program 
+CREATE DATABASE "weekend-to-do-app";
+-- Open database and create run this create table command to create appropriate table 
+CREATE TABLE "to-do"(
+    "id" SERIAL PRIMARY KEY,
+    "task" VARCHAR(300) NOT NULL,
+    "complete" BOOLEAN NOT NULL DEFAULT FALSE
+);
+-- Here is some sample data to seed the app with 
+INSERT INTO "to-do" ("task")
+VALUES ('Email Sheila'),
+    ('Meditate'),
+    ('Grocery Shop'),
+    ('Schedule Doctor Appt'),
+    ('Call Valerie'),
+    ('Pay Bills');
